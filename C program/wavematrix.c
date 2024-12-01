0b70b5ba75bc7fc543c7480990e888371be41c6d
Navdeep Singh <workwithdeepnav@gmail.com>
2024-12-01 15:46:18 +0530

    Create wavematrix.c

---
+++ C program/wavematrix.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int rows1,columns1;
+    printf("Enter a Number of Rows of Matrix 1 : ");
+    scanf("%d",&rows1);
+    printf("Enter a Number of Columns of MAtrix 1 : ");
+    scanf("%d",&columns1);
+    int arr1[rows1][columns1];
+    printf("Enter the Elements of Matrix 1 : ");
+    for(int i=0; i<rows1; i++){
+        for(int j=0; j<columns1; j++){
+            scanf("%d",&arr1[i][j]);
+        }
+    }
+    printf("Your Matrix 1 : ");
+    for(int i=0; i<rows1; i++){
+        for(int j=0; j<columns1; j++){
+            printf("%d ",arr1[i][j]);
+        }
+        printf("\n");
+    }
+    printf("You Waved Matrix : ");
+        for(int i=0; i<rows1; i++){
+            if(i%2 == 0){
+                for(int j=0; j<columns1; j++){
+                    printf("%d ",arr1[i][j]);
+                }
+                printf("\n");
+            }
+            else{
+                for(int j=columns1-1; j>=0; j--){
+                    printf("%d ",arr1[i][j]);
+                }
+                printf("\n");
+            }
+        }
+        printf("\n");
+    }
